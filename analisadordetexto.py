393a9942cd2895a162a3272b5a1c7e99abf059e2
Alexssilvadev <112443124+AlexSSilvaDev@users.noreply.github.com>
2022-11-30 10:07:03 -0300

    Add files via upload

---
+++ analisadordetexto.py
@@ -0,0 +1,14 @@
+print('o programa a seguir lê um numero de ate 4 dígitos e fala a unidade/dezena/centena e a milhar')
+num = int(input('Informe um numero : '))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+print('analisando o numero {}'.format(num))
+print('unidade : {}'.format(u))
+print('dezena : {}'.format(d))
+print('centena : {}'.format(c))
+print('milhar: {}'.format(m))
+
+
+
